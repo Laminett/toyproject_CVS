72917a75b868c0ec94221e17a9c5c92e996eafbb
shkim <shkim@alli-ex.com>
2020-06-26 15:38:36 +0900

    DB 접속정보 변경 (local -> dev)

--- db/migration/1.0.0/V1_0_0_2__create_schema.sql
+++ db/migration/1.0.0/V1_0_0_2__create_schema.sql
@@ -2,22 +2,22 @@
 CREATE TABLE posts
 (
     id            bigint AUTO_INCREMENT PRIMARY KEY,
-    created_date  datetime     NULL,
-    modified_date datetime     NULL,
     author        varchar(255) NULL,
     content       text         NOT NULL,
-    title         varchar(500) NOT NULL
+    title         varchar(500) NOT NULL,
+    created_date  datetime     NULL,
+    modified_date datetime     NULL
 );
 
 # create user
 CREATE TABLE user
 (
     id            bigint AUTO_INCREMENT PRIMARY KEY,
-    created_date  datetime     NULL,
-    modified_date datetime     NULL,
     email         varchar(255) NOT NULL,
     name          varchar(255) NOT NULL,
     picture       varchar(255) NULL,
-    role          varchar(255) NOT NULL
+    role          varchar(255) NOT NULL,
+    created_date  datetime     NULL,
+    modified_date datetime     NULL
 );
 
