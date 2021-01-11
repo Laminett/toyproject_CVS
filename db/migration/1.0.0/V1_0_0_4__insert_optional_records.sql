b78eafaefcd3c22fdaf7a2a9936d73554343aab6
shkim <shkim@alli-ex.com>
2021-01-11 18:12:36 +0900

    CVS-40 add optional data

--- db/migration/1.0.0/V1_0_0_4__insert_optional_records.sql
+++ db/migration/1.0.0/V1_0_0_4__insert_optional_records.sql
@@ -4,3 +4,10 @@ VALUES
 (2, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'test', 'test@test.com', null, 'ADMIN', null, null),
 (3, 'shkim', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'shkim', 'shkim@alli-ex.com', null, 'USER', null, null)
 ;
+
+INSERT INTO point (id, user_id, point, created_date, modified_date)
+VALUES
+(1, 1, 100, '2021-01-11 08:53:54', '2021-01-11 08:53:54'),
+(2, 2, 200, '2021-01-11 08:53:54', '2021-01-11 08:53:54'),
+(3, 3, 300, '2021-01-11 08:53:54', '2021-01-11 08:53:54')
+;
