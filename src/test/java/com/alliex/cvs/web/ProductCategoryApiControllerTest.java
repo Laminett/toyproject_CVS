edb3d5f0749cb9c7b8f8c996894d2ea001ba26ce
yki <pofour1@gmail.com>
2021-01-14 13:41:54 +0900

    ProductCategoryApiControllerTest 오류 수정

--- src/test/java/com/alliex/cvs/web/ProductCategoryApiControllerTest.java
+++ src/test/java/com/alliex/cvs/web/ProductCategoryApiControllerTest.java
@@ -86,7 +86,7 @@ public class ProductCategoryApiControllerTest {
         String categoryName = "categoryName updated";
         productCategoryUpdateRequest.setCategoryName(categoryName);
 
-        mvc.perform(put("/web-api/v1/products-categories/{id}", testId)
+        mvc.perform(post("/web-api/v1/products-categories/{id}", testId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(productCategoryUpdateRequest)))
                 .andDo(print())
@@ -107,7 +107,7 @@ public class ProductCategoryApiControllerTest {
         String categoryName = "categoryName updated";
         productCategoryUpdateRequest.setCategoryName(categoryName);
 
-        mvc.perform(put("/web-api/v1/products-categories/{id}", testId + 10L)
+        mvc.perform(post("/web-api/v1/products-categories/{id}", testId + 10L)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(productCategoryUpdateRequest)))
                 .andDo(print())
