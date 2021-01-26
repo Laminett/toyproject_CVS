b9c5acea52f40f03e23b7b4d021d82c8c2a81807
shkim <shkim@alli-ex.com>
2021-01-26 17:53:31 +0900

    CVS-29 use messageService

--- src/main/java/com/alliex/cvs/config/message/MessageAdvice.java
+++ src/main/java/com/alliex/cvs/config/message/MessageAdvice.java
@@ -1,30 +1,33 @@
 package com.alliex.cvs.config.message;
 
+import com.alliex.cvs.service.MessageService;
 import com.samskivert.mustache.Mustache;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
+@RequiredArgsConstructor
 @ControllerAdvice
 @ComponentScan(basePackages = "com.alliex.cvs.config.message")
 public class MessageAdvice {
 
-    @Autowired
-    private MessageSource messageSource;
+    private final MessageSource messageSource;
+
+    private final MessageService messageService;
 
     @ModelAttribute("messages")
     public Mustache.Lambda messages(Locale locale, HttpSession session) {
-        Locale lang = (Locale) session.getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
-        Locale finalLocale = lang == null ? locale : lang;
+        Locale sessionLocale = messageService.getSessionLocale(session);
+        Locale _locale = messageService.getLocale(locale, sessionLocale);
+
         return (frag, out) -> {
             String body = frag.execute();
-            String message = this.messageSource.getMessage(body, null, finalLocale);
+            String message = this.messageSource.getMessage(body, null, _locale);
             out.write(message);
         };
     }
