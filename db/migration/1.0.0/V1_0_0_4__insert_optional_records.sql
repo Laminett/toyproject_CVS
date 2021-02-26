b7842d3359036a49d1f13fdee57917c7c568a2f2
shkim <shkim@alli-ex.com>
2021-02-26 16:55:41 +0900

    CVS-82 trivial chage

--- db/migration/1.0.0/V1_0_0_4__insert_optional_records.sql
+++ db/migration/1.0.0/V1_0_0_4__insert_optional_records.sql
@@ -2,7 +2,7 @@ INSERT INTO user (id, username, password, department, full_name, email, phone_nu
 VALUES
 (1, 'admin', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'admin', 'test@test.com', null, 'ACTIVE', 'ADMIN', null, null),
 (2, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'test', 'test@test.com', null, 'ACTIVE', 'ADMIN', null, null),
-(3, 'shkim', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'shkim', 'shkim@alli-ex.com', 'ACTIVE', null, 'USER', null, null)
+(3, 'shkim', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Mobile Div', 'shkim', 'shkim@alli-ex.com', null, 'ACTIVE', 'USER', null, null)
 ;
 
 INSERT INTO point (id, user_id, point, created_date, modified_date)
