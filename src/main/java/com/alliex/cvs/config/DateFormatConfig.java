e7aaf4d553d005a4de60c83b02577ce809b1f089
maewha <maewha@gmail.com>
2021-02-27 03:39:39 +0900

    CVS-64 added deserializers

--- src/main/java/com/alliex/cvs/config/DateFormatConfig.java
+++ src/main/java/com/alliex/cvs/config/DateFormatConfig.java
@@ -1,5 +1,7 @@
 package com.alliex.cvs.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -11,17 +13,23 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class DateFormatConfig {
 
-    private static final String dateFormat = "dd-MM-yyyy";
+    private final String dateFormat = "dd-MM-yyyy";
 
-    private static final String datetimeFormat = "dd-MM-yyyy HH:mm:ss";
+    private final String datetimeFormat = "dd-MM-yyyy HH:mm:ss";
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return jacksonObjectMapperBuilder -> {
             // jacksonObjectMapperBuilder.timeZone(TimeZone.getTimeZone("UTC"));
             jacksonObjectMapperBuilder.simpleDateFormat(datetimeFormat);
+
+            // Set LocalDate format.
             jacksonObjectMapperBuilder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            jacksonObjectMapperBuilder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+
+            // Set LocalDateTime format.
             jacksonObjectMapperBuilder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(datetimeFormat)));
+            jacksonObjectMapperBuilder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(datetimeFormat)));
         };
     }
 
